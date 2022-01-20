dcc7d260901549f895296c91406008050aa486f9
Bogdan Bledea <bledea.bogdan97@gmail.com>
2022-01-20 22:25:09 +0200

    Update button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
 import { createSignal } from 'solid-js';
 import { Button } from './components/button';
-import { Modal } from './components/modal';
 
 function App() {
   const [getIsModalVisible, setIsModalVisible] = createSignal(true);
   return (
     <div>
-      <Modal
-        title="Modal title"
-        visible={getIsModalVisible()}
-        onOk={() => setIsModalVisible(false)}
-        onCancel={() => setIsModalVisible(false)}
-      >
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus obcaecati adipisci a eius, reiciendis magnam natus molestias soluta consectetur dolorum. Voluptatem soluta recusandae maiores dolores rerum repudiandae rem ducimus labore?e
-      </Modal>
+      <Button>Test</Button>
+      <Button variant='bright'>Test</Button>
+      <Button variant='ghost'>Test</Button>
+      <Button small>Test</Button>
+      <Button variant='bright' small>Test</Button>
+      <Button variant='ghost' small>Test</Button>
+      <Button disabled>Test</Button>
+      <Button variant='bright' disabled>Test</Button>
+      <Button variant='ghost' disabled>Test</Button>
     </div >
   );
 }
--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,24 +1,107 @@
 import { Component } from 'solid-js';
+import { styled } from 'solid-styled-components';
+
+type ButtonType = 'bright' | 'ghost' | 'accent';
 
 export interface ButtonProps {
+	variant?: ButtonType;
 	small?: boolean;
-	bright?: boolean;
-	ghost?: boolean;
 	onClick?: (e: Event) => void;
 	disabled?: boolean;
 }
 
-export const Button: Component<ButtonProps> = (props) => (
-	<button
-		className={`rev-btn`}
-		classList={{
-			'rev-btn-small': props.small,
-			'rev-btn-bright': props.bright,
-			'rev-btn-ghost': props.ghost
-		}}
-		onClick={props.onClick}
-		disabled={props.disabled}
+const StyledButton = styled('button') <{
+	variant: ButtonType;
+	small: boolean;
+}>`
+  box-sizing: border-box;
+  border: unset;
+  border-radius: 3px;
+  height: ${props => props.small ? '34px' : '48px'};
+  padding: 4px 20px;
+  font-size: 14px;
+  min-width: 100px;
+
+	&.bright {
+			background: ${props => props.theme.colors.bright};
+  		color: ${props => props.theme.colors.primary};
+  		box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
+
+			&:hover {
+				box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+			}
+
+			&:active {
+				border: 2px solid ${props => props.theme.colors.primary};
+  			box-shadow: unset;
+			}
+
+			&:disabled {
+				background: ${props => props.theme.colors.shade};
+  			color: rgba(44, 39, 56, 0.24);
+  			box-shadow: unset;
+			}
+	}
+
+	&.accent {
+		background: ${props => props.theme.colors.accent};
+		color: ${props => props.theme.colors.bright};
+
+		&:hover {
+			box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+		}
+
+		&:active {
+			border: 2px solid ${props => props.theme.colors.primary};
+			box-shadow: unset;
+		}
+
+		&:disabled {
+			background: ${props => props.theme.colors.shade};
+			color: rgba(44, 39, 56, 0.24);
+			box-shadow: unset;
+		}
+	}
+
+	&.ghost {
+		background: ${props => props.theme.colors.bright};
+		color: ${props => props.theme.colors.muted};
+		border: 2px solid ${props => props.theme.colors.muted};
+		box-shadow: unset;
+
+		&:hover {
+			color: ${props => props.theme.colors.accent};
+  		border-color: ${props => props.theme.colors.accent};
+  		box-shadow: unset;
+		}
+
+		&:active {
+			color: ${props => props.theme.colors.secondary};
+  		border-color: ${props => props.theme.colors.secondary};
+		}
+
+		&:disabled {
+			background: transparent;
+  		color: rgba(44, 39, 56, 0.24);
+  		border-color: rgba(44, 39, 56, 0.24);
+		}
+	}
+`;
+
+export const Button: Component<ButtonProps> = ({
+	variant = 'accent',
+	disabled = false,
+	small = false,
+	onClick,
+	children
+}) => (
+	<StyledButton
+		variant={variant}
+		onClick={onClick}
+		small={small}
+		disabled={disabled}
+		className={`${variant}`}
 	>
-		{props.children}
-	</button>
+		{children}
+	</StyledButton>
 );
