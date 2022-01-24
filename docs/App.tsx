afb9fc1e5e9d96240d7250cf752dbde2290b7fc4
Bogdan Bledea <bledea.bogdan97@gmail.com>
2022-01-24 16:54:46 +0200

    Popover component skeleton

--- docs/App.tsx
+++ docs/App.tsx
@@ -4,13 +4,34 @@ import { Container } from './Container';
 import { Legend } from './Legend';
 import branding from '../assets/images/branding.svg';
 import { Colors } from './Colors';
+import { Popover } from '../components/popover/Popover';
 
 const App = () => (
   <div>
-    <Container type='full'>
+    <Container type='full' padding='0'>
       <img src={branding} alt='RevkitUI' width='100%' />
     </Container>
     <Legend title="Typeface" rank={1} />
+    <Container type="fluid">
+      {/* <Popover title='Popover Title' description='Supportive text for the top popover.' placement='bottom'>
+        Test
+      </Popover>
+      <Popover title='Popover Title' description='Supportive text for the top popover.' placement='top'>
+        Test
+      </Popover> */}
+      <Popover title='Popover Title' description='Supportive text for the top popover.' placement='top'>
+        Top placement
+      </Popover>
+      <Popover title='Popover Title' description='Supportive text for the top popover.' placement='bottom'>
+        Bottom placement
+      </Popover>
+      <Popover title='Popover Title' description='Supportive text for the top popover.' placement='left'>
+        Left placement
+      </Popover>
+      <Popover title='Popover Title' description='Supportive text for the top popover.' placement='right'>
+        Right placement
+      </Popover>
+    </Container>
     <Legend title="Colors" rank={2} />
     <Colors />
     <Legend title="Icons" rank={3} />
