d782b5554b39ca307ca055e9c1f8e56c3b3703a2
Bogdan Bledea <bledea.bogdan97@gmail.com>
2022-01-26 17:42:22 +0200

    Update docs project

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -1,11 +1,13 @@
+import path from 'path';
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 
 export default defineConfig({
 	plugins: [solidPlugin()],
 	build: {
+		outDir: 'public',
+		assetsDir: '.',
 		target: 'esnext',
-		
 		polyfillDynamicImport: false,
 	},
 });
