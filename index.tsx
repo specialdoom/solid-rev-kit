f32e448bfd27b27df314f0264379645afdd41b31
Bogdan Bledea <bledea.bogdan97@gmail.com>
2022-01-29 14:14:58 +0200

    Export more components

--- index.tsx
+++ index.tsx
@@ -1,11 +1,29 @@
 import { Button } from './src/components/button';
+import { Heading, Paragraph, Label } from './src/components/typography';
 import { Alert } from './src/components/alert';
 import { RevKitTheme } from './src/components/themeProvider/ThemeProvider';
 import { theme } from './src/components/themeProvider/theme';
+import { Avatar } from './src/components/avatar';
+import { Callout } from './src/components/callout';
+import { Card } from './src/components/card';
+import { Progress } from './src/components/progress';
+import { Space } from './src/components/space';
+import { Spinner } from './src/components/spinner';
+import { Tag } from './src/components/tag';
 
 export {
-	theme,
 	Button,
 	Alert,
-	RevKitTheme
+	Heading,
+	Paragraph,
+	Label,
+	Avatar,
+	Card,
+	Callout,
+	Progress,
+	Spinner,
+	Space,
+	Tag,
+	RevKitTheme,
+	theme
 };
