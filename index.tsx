5e61bb922093fdd3602cc71497dca6c8b86c86b8
Bogdan Bledea <bledea.bogdan97@gmail.com>
2022-01-29 14:05:38 +0200

    export default theme

--- index.tsx
+++ index.tsx
@@ -1,8 +1,10 @@
 import { Button } from './src/components/button';
 import { Alert } from './src/components/alert';
 import { RevKitTheme } from './src/components/themeProvider/ThemeProvider';
+import { theme } from './src/components/themeProvider/theme';
 
 export {
+	theme,
 	Button,
 	Alert,
 	RevKitTheme
